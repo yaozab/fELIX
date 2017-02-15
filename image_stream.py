dd3b7c835943830a44df7198333b1ed48fbdc56d
YaoYao <ybai36@gatech.edu>
2017-02-15 18:00:27 -0500

    fdfsa

--- image_stream.py
+++ image_stream.py
@@ -45,9 +45,9 @@ class image_stream:
     #(rows,cols,channels) = cv_image.shape
     #if cols > 60 and rows > 60 :
     #    cv2.circle(cv_image, (50,50), 10, 255)
-    depth_array = np.array(cv_depthimage, dtype=np.float32)
-    print(depth_array)
-    depth_norm = cv2.normalize(depth_array, alpha=0, beta=255)
+    #depth_array = np.array(cv_depthimage, dtype=np.float32)
+    #print(depth_array)
+    depth_norm = cv2.normalize(cv_depthimage, alpha=0, beta=255)
     cv2.imshow("depth", depth_norm)
     cv2.waitKey(3)
     try:
