f692a2661bbcdaec509c19f8a3f188d72698563c
YaoYao <ybai36@gatech.edu>
2017-02-21 20:40:29 -0500

    itfd

--- goforward_smart.py
+++ goforward_smart.py
@@ -31,20 +31,20 @@ class GoForward():
   def __init__(self):
     # initiliaze
     rospy.init_node('GoForward', anonymous=False)
-    # tell user how to stop TurtleBot
-    rospy.loginfo("To stop TurtleBot CTRL + C")
-    # What function to call when you ctrl + c    
-    rospy.on_shutdown(self.shutdown())
-    # Create a publisher which can "talk" to TurtleBot and tell it to move
-    # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
-    self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
-    # create subscribers
-    rospy.Subscriber("/mobile_base/events/bumper",BumperEvent,self.BumperEventCallback)
-    rospy.Subscriber("/mobile_base/events/wheel_drop",WheelDropEvent,self.WheelDropEventCallback)
-    self.stateMachine = FSM()
+  # tell user how to stop TurtleBot
+  rospy.loginfo("To stop TurtleBot CTRL + C")
+  # What function to call when you ctrl + c    
+  rospy.on_shutdown(self.shutdown())
+  # Create a publisher which can "talk" to TurtleBot and tell it to move
+  # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
+  self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+  # create subscribers
+  rospy.Subscriber("/mobile_base/events/bumper",BumperEvent,self.BumperEventCallback)
+  rospy.Subscriber("/mobile_base/events/wheel_drop",WheelDropEvent,self.WheelDropEventCallback)
+  self.stateMachine = FSM()
 
-  	#TurtleBot will stop if we don't keep telling it to move.  How often should we tell it to move? 10 HZ
-    r = rospy.Rate(10);
+	#TurtleBot will stop if we don't keep telling it to move.  How often should we tell it to move? 10 HZ
+  r = rospy.Rate(10);
 
   if (self.stateMachine.getCurrentState() == 'Hit Left'):
     # back, turn right, pause
