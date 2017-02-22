d59d8965976f3f84625dde93b1e62f387e94a03b
YaoYao <ybai36@gatech.edu>
2017-02-21 21:02:11 -0500

    spin init

--- gosmartforward.py
+++ gosmartforward.py
@@ -31,6 +31,7 @@ class GoForward():
     def __init__(self):
         # initiliaze
         rospy.init_node('GoForward', anonymous=False)
+        rospy.spin()
 
 	# tell user how to stop TurtleBot
 	rospy.loginfo("To stop TurtleBot CTRL + C")
@@ -74,7 +75,7 @@ class GoForward():
           move_cmd.linear.x = 0.2
           # let's turn at 0 radians/s
           move_cmd.angular.z = 0
-        rospy.spin()
+
 
     	# as long as you haven't ctrl + c keeping doing...
         while not rospy.is_shutdown():
