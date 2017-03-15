9eef956d841037019f3b1bf6bdda3a95a39141f1
turtlebot <turtlebot@turtlebot.(none)>
2017-03-15 15:42:36 -0400

    blah

--- follow_the_gap.py
+++ follow_the_gap.py
@@ -9,22 +9,22 @@ from FSM import FSM
 from kobuki_msgs.msg import BumperEvent
 from kobuki_msgs.msg import WheelDropEvent
 from kobuki_msgs.msg import CliffEvent
+
 class follow_the_gap:
     def __init__(self):
 		'''Initializes an object of this class.
 		The constructor creates a publisher, a twist message.
 		3 integer variables are created to keep track of where obstacles exist.
 		3 dictionaries are to keep track of the movement and log messages.'''
-		rospy.init_node(follow_the_gap)
+		rospy.init_node('follow_the_gap', anonymous=True)
 		rospy.loginfo("follow the gap starting")
-		self.sub = rospy.Subscriber('/scan', LaserScan, call_back)
+		self.sub = rospy.Subscriber('/scan', LaserScan, self.call_back)
 		self.pub = rospy.Publisher('/gapscan',LaserScan, queue_size = 10)
 		self.gapArray = []
 		self.obstacles = []
-		self.r = rospy.Rate(5)
-		
+		self.r = rospy.Rate(5)		
 
-    def callback(self,laserscan):
+    def call_back(self,laserscan):
 		'''Passes laserscan onto function sort which gives the sect
 		variables the proper values.  Then the movement function is run
 		with the class sect variables as parameters.
@@ -33,26 +33,26 @@ class follow_the_gap:
 		entries = len(laserscan.ranges)
 		# make binary array, one if obstacle 0 if free
 		binLaserscan = laserscan
-		objectFound = 0
+		laserArr = np.zeros(entries)
 		for entry in range(0,entries):
-			if laserscan[entry] > 1.5 or math.isnan(laserscan[entry]):
-				binLaserscan[entry] = 13
+			if (laserscan.ranges[entry] > 1.5):
+				laserArr[entry] = laserscan.range_max
 			else:
-				binLaserscan[entry] = 1
-
-		pub.publish(binLaserscan)
+				laserArr[entry] = laserscan.range_min
+		binLaserscan.ranges = laserArr
+		self.pub.publish(binLaserscan)
 		self.r.sleep()
 
-	def shutdown(self):
+    def shutdown(self):
 		rospy.loginfo("shutdown")
 		rospy.sleep(1)
 
 if __name__ == "__main__":
 	'''A Scan_msg class object called sub_obj is created and listener
 	function is run'''
-		follow = follow_the_gap()
-		rospy.on_shutdown(follow.shutdown)
-		try:
-			rospy.spin()
-		except KeyboardInterrupt:
-			print("shutting down")
+        follow = follow_the_gap()
+        rospy.on_shutdown(follow.shutdown)
+        try:
+            rospy.spin()
+        except KeyboardInterrupt:
+            print("shutting down")
