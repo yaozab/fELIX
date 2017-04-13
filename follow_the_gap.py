a0d16f2fa291e97abfdeae2d114cd867992ade13
turtlebot <turtlebot@turtlebot.(none)>
2017-04-13 18:32:42 -0400

    stuff

--- follow_the_gap.py
+++ follow_the_gap.py
@@ -9,6 +9,7 @@ from FSM import FSM
 from kobuki_msgs.msg import BumperEvent
 from kobuki_msgs.msg import WheelDropEvent
 from kobuki_msgs.msg import CliffEvent
+from nav_msgs.msg import Odometry
 
 class follow_the_gap:
     def __init__(self):
@@ -22,32 +23,38 @@ class follow_the_gap:
 		self.sub = rospy.Subscriber('/scan', LaserScan, self.laser_call_back)
 		self.pub = rospy.Publisher('/gapscan',LaserScan, queue_size = 10)
 		self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
+		self.orientation = rospy.Subscriber('/odom', Odometry, self.odom_callback)
 		#rospy.Subscriber("/mobile_base/events/bumper",BumperEvent,self.BumperEventCallback)
-    	#rospy.Subscriber("/mobile_base/events/wheel_drop",WheelDropEvent,self.WheelDropEventCallback)
+	    	#rospy.Subscriber("/mobile_base/events/wheel_drop",WheelDropEvent,self.WheelDropEventCallback)
 
 		self.gapArray = []
 		self.obstacles = []
-		self.r = rospy.Rate(5)
+		self.r = rospy.Rate(10)
 		self.forwardSpeed = 0.2
-		self.angularSpeed = 0.1	
-	'''
-	# callback functions
-	def BumperEventCallback(self, data):
-		if (data.state == BumperEvent.PRESSED and data.bumper == BumperEvent.LEFT):
-			self.stateMachine.setCurrentState('Hit Left')
-		elif (data.state == BumperEvent.PRESSED and data.bumper == BumperEvent.RIGHT):
-			self.stateMachine.setCurrentState('Hit Right')
-		elif (data.state == BumperEvent.PRESSED and data.bumper == BumperEvent.CENTER):
-			self.stateMachine.setCurrentState('Hit Center')
-		else:
-			self.stateMachine.setCurrentState('Go Forward')
+		self.angularSpeed = 2
+		self.startX = None
+		self.startY = None
+		self.startZ = None
+		
+		self.currX = 0
+		self.currY = 0
+		self.currZ = 0
+		
+		self.endX = 0
 
-	def WheelDropEventCallback(self,data):
-		if (data.state == WheelDropEvent.DROPPED):
-			self.stateMachine.setCurrentState('Wheel Drop')
-		else:
-			self.stateMachine.setCurrentState('Go Forward')
-	'''
+    def odom_callback(self, data):
+	    	if self.startX == None:
+	    		self.startX = data.pose.pose.position.x
+	    		self.endX = self.startX - 15
+	    	if self.startY == None:
+	    		self.startY = data.pose.pose.position.y
+	    	if self.startZ == None:
+	        	self.startZ = data.pose.pose.orientation.z
+	    
+	    	self.currX = data.pose.pose.position.x
+	    	self.currY = data.pose.pose.position.y
+		self.currZ = data.pose.pose.orientation.z
+		
     def laser_call_back(self,laserscan):
 		'''Passes laserscan onto function sort which gives the sect
 		variables the proper values.  Then the movement function is run
@@ -63,18 +70,22 @@ class follow_the_gap:
 		nanStartIndex = 0
 		laserArr = np.zeros(entries)
 		for entry in range(0,entries):
-			if (laserscan.ranges[entry] > 0.5):
+			if (laserscan.ranges[entry] > 1.5) or np.isnan(laserscan.ranges[entry]):
 				#print(entry)
 				laserArr[entry] = laserscan.range_max
-			else:															
-				if (laserArr[entry - 1] == laserscan.range_max):
-					if (entry + 1) <= entries:
-						if laserscan.ranges[entry+1] > 0.5:
-							laserArr[entry] = laserscan.range_max
-						else:
-							laserArr[entry] = laserscan.range_min
-				else:
-					laserArr[entry] = laserscan.range_min
+			#elif np.isnan(laserscan.ranges[entry]):
+			#    laserArr[entry] = laserscan.range_max
+			else:
+			    laserArr[entry] = laserscan.range_min															
+				#if (laserArr[entry - 1] == laserscan.range_max):
+				#	if (entry + 1) <= entries:
+				#		if laserscan.ranges[entry+1] > 0.5:
+				#			laserArr[entry] = laserscan.range_max
+				#		else:
+				#			laserArr[entry] = laserscan.range_min
+				
+				#else:
+				#	laserArr[entry] = laserscan.range_min
 
 		# making the gap index
 		previousVal = 0
@@ -95,27 +106,53 @@ class follow_the_gap:
 		for gap in gapIndex:
 			gapSize.append(gap[1] - gap[0])
 		#print(gapSize, np.argmax(gapSize))
-		largestGapIndexes = gapIndex[np.argmax(gapSize)]
-		center = ceil(entries/2)
-		leftDiff = center - largestGapIndexes[0]
-		rightDiff = center - largestGapIndexes[1]
-		middleDiff = ceil((leftDiff + rightDiff)/2)
-		gapCenterAngle = -middleDiff * laserscan.angle_increment
+		if len(gapIndex) < 1:
+		    gapCenterAngle = 0.1
+		else:
+		    largestGapIndexes = gapIndex[np.argmax(gapSize)]
+		    center = ceil(entries/2)
+		    leftDiff = center - largestGapIndexes[0]
+		    rightDiff = center - largestGapIndexes[1]
+		    middleDiff = ceil((leftDiff + rightDiff)/2)
+		    gapCenterAngle = -middleDiff * laserscan.angle_increment
 		
 		#print(min(laserscan.ranges))
 		#print (entries, largestGapIndexes, leftDiff, rightDiff, middleDiff, gapCenterAngle)
 		print('Gap center angle: ', math.degrees(gapCenterAngle))
-		
-		if math.abs(math.degrees(gapCenterAngle)) < 15:
-			# if less than 15 deg, go forward
+		'''
+		if self.currX == self.endX and self.currY == self.startY and self.currZ == self.startZ:
+			print('at goal, shutting down')
+			self.shutdown()
+		elif self.currX == self.endX and self.currY == self.startY:
+		    # need to turn to correct angle
+			angleDiff2 = self.currZ - self.startZ
 			move = Twist()
-			move.linear.x = self.forwardSpeed
-			self.cmd_vel.Publish(move)
+			move.angular.z = angleDiff2
+			self.cmd_vel.publish(move)
+		elif self.currX == self.endX:
+		    # move back to y location
+			print('movey')
+			
 		else:
-			angSpeed = gapCenterAngle * self.turnSpeed
-			move = Twist()
-			move.angular.z = angSpeed
-			self.cmd_vel.Publish(move)
+		'''
+		if True:
+			if np.abs(math.degrees(gapCenterAngle)) < 5:
+				# if less than 15 deg, go forward
+				print('moving forward')
+				move = Twist()
+				#if self.currY != self.startY:
+				#    #print(self.currY, self.startY)
+				#    difference = self.startY-self.currY
+				#    move.angular.z = difference / 2
+				move.linear.x = self.forwardSpeed
+				self.cmd_vel.publish(move)
+			else:
+		    		angSpeed = gapCenterAngle
+		    		print('angular speed', angSpeed)
+		    		move = Twist()
+		    		move.angular.z = angSpeed * 1.2
+		    		move.linear.x = 0.1
+		    		self.cmd_vel.publish(move)
 
 		binLaserscan.ranges = laserArr
 		self.pub.publish(binLaserscan)
